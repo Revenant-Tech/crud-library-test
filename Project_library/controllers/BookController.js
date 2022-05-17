491747f9bc2ae76df4bf115d7a0d65c5f5afcce0
R <rodrigoraccuia@unesc.net>
2022-05-17 18:14:19 -0300

    ONLY MISSING UPDATE

--- Project_library/controllers/BookController.js
+++ Project_library/controllers/BookController.js
@@ -22,7 +22,38 @@ module.exports = class BookController {
 
     await Books.create(book);
 
+
     res.redirect("/");
   }
+
+  static async removeBook(req, res) {
+    const id = req.body.id;
+
+    await Books.destroy({ where: { id: id } });
+
+    res.redirect("/");
+  }
+
+  static async updateBook(req, res) {
+    const id = req.params.id;
+
+    const book = await Book.findOne({ where: { id: id }, raw: true });
+
+    res.render("/edit", { book });
+  }
+
+  static async updateBookPost(req, res) {
+    const id = req.body.id;
+
+    const book = {
+      title: req.body.title,
+      pages: req.body.pages,
+    };
+
+    await Book.update(book, { where: { id: id } });
+
+    res.redirect("/");
+  }
+
 
 };
--- Project_library/routes/booksRoutes.js
+++ Project_library/routes/booksRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const BookController = require("../controllers/BookController");
 
-router.get('/add', BookController.createBook)
-router.post('/add', BookController.createBookSave)
+router.get('/add', BookController.createBook);
+router.post('/add', BookController.createBookSave);
+router.post('/remove', BookController.removeBook);
+router.get('/edit/:id', BookController.updateBook)
+router.post('/edit', BookController.updateBookPost)
+router.get('/', BookController.showBooks)
 router.get("/", BookController.showBooks);
 
 module.exports = router;
